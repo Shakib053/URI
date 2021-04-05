5a3f4d94889f7366b10f41c1f45b713e02e5952d
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-06 01:31:31 +0600

    Electrical Outlet

---
+++ 1930.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long
+#define pi 3.1416
+#define M 10000000
+int main()
+{
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<a+b+c+d-3<<endl;
+	return 0;
+}
