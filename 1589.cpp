40d23ea404a9b87a85e4032cc03c7a0f153338d9
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-06 02:23:38 +0600

    Bob Conduit

---
+++ 1589.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long
+#define pi 3.1416
+#define M 10000000
+int main()
+{   
+    int t;
+    int a,b;
+    cin>>t;
+    while (t--)
+    {
+      
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+    
+	return 0;
+}
