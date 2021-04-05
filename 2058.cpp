4fb34dca21b2d381b092077e1a11c74f8fa69d1f
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-06 02:14:51 +0600

    Triangles and Regular Polygons

---
+++ 2058.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long
+#define pi 3.1416
+#define M 10000000
+int main()
+{  
+    ll int a;
+    cin>>a;
+    cout<<a-2<<endl;
+	return 0;
+}
