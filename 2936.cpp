89a291947f762fe1b7e42b7e8ed676c2d0dd393d
Kazi Tanjim Shakib <75584294+Shakib053@users.noreply.github.com>
2021-04-06 01:54:15 +0600

    How Much Cassava?

---
+++ 2936.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long
+#define pi 3.1416
+#define M 10000000
+int main()
+{
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    cout<<a*300+b*1500+c*600+d*1000+e*150+225<<endl;
+	return 0;
+}
