279faf482f6a183c635760b0e6932fb1df906e40
Shakib053 <75584294+Shakib053@users.noreply.github.com>
2021-04-05 20:33:59 +0600

    Above the Secundary Diagonal

---
+++ 1185.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long
+#define pi 3.1416
+#define M 10000000
+int main()
+{  
+    double a[12][12];
+    char k;
+    double sum=0;
+    cin>>k;
+    for(int i=0;i<12;i++)
+    {
+        for(int j=0;j<12;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<11;i++)
+        {
+            for(int j=0;j<11-i;j++)
+            
+            {
+                sum=sum+a[i][j];
+            }
+        }
+    if(k=='S')
+    {
+        cout<<sum<<endl;
+    }
+    else if(k=='M')
+    cout<<fixed<<setprecision(1)<<sum/66<<endl;
+   
+	return 0;
+}
